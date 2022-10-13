faf4f96d34c367a519b511035bf82f526f5a9157
황아영 <ayoganatanatda@naver.com>
2022-10-13 20:21:23 +0900

    Establish: 프로젝트 환경 설정

---
+++ worldcrossword/src/test/java/com/worldcrossword/WorldcrosswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldcrossword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldcrosswordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
