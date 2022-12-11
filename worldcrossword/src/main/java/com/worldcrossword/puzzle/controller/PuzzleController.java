a5c8e59a109be8f229e6791d9b7a55e185aedb56
nexusbusim <ilov1112@gmail.com>
2022-12-11 23:26:28 +0900

    Merge 오류 수정

--- worldcrossword/src/main/java/com/worldcrossword/puzzle/controller/PuzzleController.java
+++ worldcrossword/src/main/java/com/worldcrossword/puzzle/controller/PuzzleController.java
@@ -2,8 +2,9 @@ package com.worldcrossword.puzzle.controller;
 
 import com.worldcrossword.puzzle.dto.PuzzleRequest;
 import com.worldcrossword.puzzle.entity.DictionaryEntity;
+import com.worldcrossword.puzzle.entity.PuzzleEntity;
 import com.worldcrossword.puzzle.service.interfaces.PuzzleService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,20 +12,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/puzzle")
 public class PuzzleController {
 
-    private final PuzzleService puzzleService;
+    @Autowired
+    PuzzleService puzzleService;
 
     @GetMapping("/{sessionName}")
-    public ResponseEntity<List<PuzzleEntity>> getPuzzle(@PathVariable String sessionName) {
-        return new ResponseEntity<>(puzzleService.getPuzzle(sessionName), HttpStatus.OK);
+    public ResponseEntity<PuzzleRequest> getPuzzle(@PathVariable String sessionName) {
+        return new ResponseEntity<>(PuzzleRequest.builder()
+                .puzzle(puzzleService.getPuzzle(sessionName))
+                .user(puzzleService.getUsers(sessionName))
+                .build(), HttpStatus.OK);
     }
 
     @GetMapping("/mean/{word}")
-    public ResponseEntity<DictionaryEntity> getMean(@PathVariable String word) {
+    public ResponseEntity<DictionaryEntity> getWord(@PathVariable String word) {
         try {
             return new ResponseEntity<>(puzzleService.getWord(word), HttpStatus.OK);
         } catch (Exception e) {
