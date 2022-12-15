bc1c6af79915c7eb1e9b39f0bb76201c63475b7b
황아영 <ayoganatanatda@naver.com>
2022-12-15 19:47:41 +0900

    Feat: PuzzleServiceImpl 수정

--- worldcrossword/src/main/java/com/worldcrossword/puzzle/service/PuzzleServiceImpl.java
+++ worldcrossword/src/main/java/com/worldcrossword/puzzle/service/PuzzleServiceImpl.java
@@ -3,6 +3,7 @@ package com.worldcrossword.puzzle.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.worldcrossword.member.service.MemberService;
 import com.worldcrossword.puzzle.dto.PuzzleSolveDto;
 import com.worldcrossword.puzzle.dto.SessionRequestResDto;
 import com.worldcrossword.puzzle.entity.DictionaryEntity;
@@ -17,18 +18,18 @@ import com.worldcrossword.puzzle.repository.UserRepository;
 import com.worldcrossword.puzzle.service.interfaces.PuzzleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.time.Duration;
+import java.util.*;
+
 
 @Service
 @RequiredArgsConstructor
@@ -37,6 +38,13 @@ import java.util.Optional;
 public class PuzzleServiceImpl implements PuzzleService {
 
     private final ObjectMapper objectMapper;
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final String MemberToSessionPrefix = "MI:SN";
+    private final String MemberToScorePrefix = "MI:SC";
+    private final String SessionNameToSessionIds = "SN:SIs";
+    private final int USER_SESSION_DURATION = 3600 * 24 * 100;
+    private final int USER_SCORE_DURATION = 3600 * 24 * 100;
 
     public String objToJson(SessionRequestResDto obj) {
 
@@ -152,9 +160,9 @@ public class PuzzleServiceImpl implements PuzzleService {
     }
 
     @Override
-    public Boolean solvePuzzle(PuzzleSolveDto puzzleSolveDto, String sessionName) throws IOException {
+    public Boolean solvePuzzle(PuzzleSolveDto puzzleSolveDto, Long memberId) throws IOException {
         // 세션에 들어있는 퍼즐 전부 찾아옴.
-        List<PuzzleEntity> puzzles = puzzleRepository.findAllBySessionName(sessionName);
+        /*List<PuzzleEntity> puzzles = puzzleRepository.findAllBySessionName(sessionName);
         UserEntity user = userRepository.findByGoogleId(puzzleSolveDto.getGoogleId()).orElseThrow();
 
         Long row = puzzleSolveDto.getRow();
@@ -175,45 +183,77 @@ public class PuzzleServiceImpl implements PuzzleService {
                     break;
                 }
             }
-        }
+        }*/
+
+        PuzzleEntity puzzle = puzzleRepository.findById(puzzleSolveDto.getId())
+                .orElseThrow(() -> new RuntimeException("퍼즐이 없습니다."));
 
         // 퍼즐이 없으면 푸려고 시도한 사람에게 없다고 보냄.
-        if(puzzle == null) {
+        /*if(puzzle == null) {
             for(int i = 0;i < PuzzleWebsocket.CLIENTS.size();i++) {
                 if(PuzzleWebsocket.CLIENTS.get(i).getId().equals(user.getSessionId())) {
                     PuzzleWebsocket.CLIENTS.get(i).sendMessage(new TextMessage(objToJson(SessionRequestResDto.builder().stat("false").message("찾는 퍼즐이 없습니다.").sessionName(sessionName).build())));
                 }
             }
             return false;
-        }
+        }*/
 
         if(puzzle.getWord().equals(puzzleSolveDto.getWord())) {
-            puzzle.successPuzzle();
-            puzzleRepository.save(puzzle);
+            String MI_SI_key = MemberToSessionPrefix + memberId;
+            String MI_SC_key = MemberToScorePrefix + memberId;
+            String sessionName = (String) Optional.ofNullable(redisTemplate.opsForValue().get(MI_SI_key))
+                            .orElseThrow(() -> new RuntimeException("세션이 없습니다."));
+            Long score = (Long) Optional.ofNullable(redisTemplate.opsForValue().get(MI_SC_key))
+                            .orElseThrow(() -> new RuntimeException("점수가 없습니다."));
+            redisTemplate.opsForValue().set(MI_SC_key, score + 100);
+            redisTemplate.expire(MI_SC_key, Duration.ofSeconds(USER_SCORE_DURATION));
+            Long updateScore = (Long) Optional.ofNullable(redisTemplate.opsForValue().get(MI_SC_key))
+                            .orElseThrow(() -> new RuntimeException("점수 업데이트 실패"));
+            if(updateScore != score + 100) throw new RuntimeException("점수 업데이트 실패");
 
-            user.changeSolve(false, null);
-            userRepository.save(user);
+            String SN_SIs_key = SessionNameToSessionIds + sessionName;
+            Set<Object> sessionIds = Optional.ofNullable(redisTemplate.opsForSet().members(SN_SIs_key))
+                    .orElseThrow(() -> new RuntimeException("세션 id를 가져오는 데 실패했습니다."));
+
+            puzzle.successPuzzle();
+//            puzzleRepository.save(puzzle); // 엔티티 수정하면 따로 permit 할 필요 없이 스냅샷이랑 비교해서 수정사항 반영해주는걸로 알고 있습니다.
+
+//            user.changeSolve(false, null);
+//            userRepository.save(user);
 
             // 나 자신이 아니고, 현재 접속중인 유저 중 하나이면서, 퍼즐 세션이 일치하는 유저들에게 not_solving task로 풀이 끝났음을 알림
             // 본인에게도 정답임을 알림.
-            for(int i = 0;i < PuzzleWebsocket.CLIENTS.size();i++) {
-                if (PuzzleWebsocket.CLIENTS.get(i).getId().equals(user.getSessionId())) {
-                    PuzzleWebsocket.CLIENTS.get(i).sendMessage(new TextMessage(objToJson(SessionRequestResDto.builder().stat("true").message("정답입니다.").build())));
-                } else if (userRepository.findBySessionId(PuzzleWebsocket.CLIENTS.get(i).getId()).isPresent() && userRepository.findBySessionId(PuzzleWebsocket.CLIENTS.get(i).getId()).get().getSessionName().equals(sessionName)) {
-                    PuzzleWebsocket.CLIENTS.get(i).sendMessage(new TextMessage(objToJson(SessionRequestResDto.builder().stat("true_answer").word(puzzle.getWord()).build())));
-                }
+//            for(int i = 0;i < PuzzleWebsocket.CLIENTS.size();i++) {
+//                if (PuzzleWebsocket.CLIENTS.get(i).getId().equals(sessionId)) {
+//                    PuzzleWebsocket.CLIENTS.get(i).sendMessage(new TextMessage(objToJson(SessionRequestResDto.builder().stat("true").message("정답입니다.").build())));
+//                } else if (userRepository.findBySessionId(PuzzleWebsocket.CLIENTS.get(i).getId()).isPresent() && userRepository.findBySessionId(PuzzleWebsocket.CLIENTS.get(i).getId()).get().getSessionName().equals(puzzleSolveDto.getSessionName())) {
+//                    PuzzleWebsocket.CLIENTS.get(i).sendMessage(new TextMessage(objToJson(SessionRequestResDto.builder().stat("true_answer").word(puzzle.getWord()).build())));
+//                }
+//            }
+
+            for(WebSocketSession user : PuzzleWebsocket.CLIENTS) {
+                if(sessionIds.contains(user.getId())) user.sendMessage(
+                        new TextMessage(objToJson(
+                                SessionRequestResDto.builder()
+                                        .puzzle(puzzle)
+                                        .stat(SessionRequestResDto.State.SOLVED)
+                                        .build()
+                        ))
+                );
             }
+
             return true;
-        } else {
-            for(int i = 0;i < PuzzleWebsocket.CLIENTS.size();i++) {
-                if (PuzzleWebsocket.CLIENTS.get(i).getId().equals(user.getSessionId())) {
-                    PuzzleWebsocket.CLIENTS.get(i).sendMessage(new TextMessage(objToJson(SessionRequestResDto.builder().stat("false").message("오답입니다.").build())));
-                } else if (userRepository.findBySessionId(PuzzleWebsocket.CLIENTS.get(i).getId()).isPresent() && userRepository.findBySessionId(PuzzleWebsocket.CLIENTS.get(i).getId()).get().getSessionName().equals(sessionName)) {
-                    PuzzleWebsocket.CLIENTS.get(i).sendMessage(new TextMessage(objToJson(SessionRequestResDto.builder().stat("false_answer").word(puzzle.getWord()).build())));
-                }
-            }
-            return false;
         }
+//        else {
+//            for(int i = 0;i < PuzzleWebsocket.CLIENTS.size();i++) {
+//                if (PuzzleWebsocket.CLIENTS.get(i).getId().equals(user.getSessionId())) {
+//                    PuzzleWebsocket.CLIENTS.get(i).sendMessage(new TextMessage(objToJson(SessionRequestResDto.builder().stat("false").message("오답입니다.").build())));
+//                } else if (userRepository.findBySessionId(PuzzleWebsocket.CLIENTS.get(i).getId()).isPresent() && userRepository.findBySessionId(PuzzleWebsocket.CLIENTS.get(i).getId()).get().getSessionName().equals(sessionName)) {
+//                    PuzzleWebsocket.CLIENTS.get(i).sendMessage(new TextMessage(objToJson(SessionRequestResDto.builder().stat("false_answer").word(puzzle.getWord()).build())));
+//                }
+//            }
+            return false;
+//        }
     }
 
 
