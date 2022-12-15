33aa31d230fba7754bf76c71b33f3a4ea7b5c71b
jelly nexus <ilov1112@gmail.com>
2022-12-15 12:30:58 +0000

    goorm merge error cleared

--- worldcrossword/src/main/java/com/worldcrossword/google/controller/GoogleController.java
+++ worldcrossword/src/main/java/com/worldcrossword/google/controller/GoogleController.java
@@ -13,12 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-<<<<<<< HEAD
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-=======
->>>>>>> 762650f49c084a918894c2994d4fc5f25a199c7f
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
