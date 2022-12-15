0e1d7d298c775be5fd8a9ac5e3fc5a2141ca1c05
황아영 <ayoganatanatda@naver.com>
2022-12-15 19:49:24 +0900

    Feat: SolveDTO 추가

---
+++ worldcrossword/src/main/java/com/worldcrossword/puzzle/dto/SolveDTO.java
@@ -0,0 +1,14 @@
+package com.worldcrossword.puzzle.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SolveDTO {
+
+    private Boolean solved;
+
+}
