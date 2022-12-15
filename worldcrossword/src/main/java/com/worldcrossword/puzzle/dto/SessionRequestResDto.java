556b4ad471d47c0fffb1e936d1ebd30fe77ced37
황아영 <ayoganatanatda@naver.com>
2022-12-15 19:49:10 +0900

    Feat: SessionRequestResDto 수정

--- worldcrossword/src/main/java/com/worldcrossword/puzzle/dto/SessionRequestResDto.java
+++ worldcrossword/src/main/java/com/worldcrossword/puzzle/dto/SessionRequestResDto.java
@@ -1,18 +1,24 @@
 package com.worldcrossword.puzzle.dto;
 
+import com.worldcrossword.puzzle.entity.PuzzleEntity;
 import lombok.*;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @ToString
 public class SessionRequestResDto {
     // true, false, solving, not_solving
-    private String stat;
-    private String sessionName;
+//    private String stat;
+//    private String sessionName;
+    private PuzzleEntity puzzle;
+    private State stat;
     private String message;
+//
+//    private String word;
 
-    private String word;
+    public enum State {
+        SOLVED, NOT_SOLVED, TRUE, FALSE
+    }
 }
